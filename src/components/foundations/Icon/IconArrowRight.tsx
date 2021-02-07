aea8cf3a74529fa8adbb30a3f572522892bb5624
Lorenzo García Moreno <dev@lorenzogm.com>
2021-02-07 16:24:50 +0100

    feat(Icon): create Icon folder

---
+++ src/components/foundations/Icon/IconArrowRight.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react'
+
+const IconArrowRight: FC = () => {
+  return (
+    <svg
+      className="h-5 w-5 mx-2"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <title>Arrow right icon</title>
+      <path d="M17 8l4 4m0 0l-4 4m4-4H3" />
+    </svg>
+  )
+}
+
+export default IconArrowRight
---
+++ src/components/foundations/Icon/IconClose.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react'
+
+const IconClose: FC = () => {
+  return (
+    <svg
+      className="h-5 w-5"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <title>Close icon</title>
+      <path d="M6 18L18 6M6 6l12 12" />
+    </svg>
+  )
+}
+
+export default IconClose
