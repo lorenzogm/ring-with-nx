44d0e0b3da4d527fcb46010985eae37cafce175a
Lorenzo García Moreno <dev@lorenzogm.com>
2021-02-21 23:10:58 +0100

    style(BoxHeader)

--- src/components/atoms/BoxHeader.tsx
+++ src/components/atoms/BoxHeader.tsx
@@ -1,7 +1,12 @@
 import { ReactElement } from 'react'
+import styled from 'styled-components'
 import Box from '@material-ui/core/Box'
 import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider'
+
+const TypographyStyled = styled(Typography)`
+  text-transform: uppercase;
+`
 
 type BoxHeaderProps = {
   children: string
@@ -10,7 +15,7 @@ type BoxHeaderProps = {
 export default function BoxHeader({ children }: BoxHeaderProps): ReactElement {
   return (
     <Box mb={2}>
-      <Typography variant="h5">{children}</Typography>
+      <TypographyStyled variant="h6">{children}</TypographyStyled>
       <Divider />
     </Box>
   )
