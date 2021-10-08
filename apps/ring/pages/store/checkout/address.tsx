8093660d53836328ad5ae6eb920c921ce448cee7
Lorenzo García Moreno <dev@lorenzogm.com>
2021-10-09 00:38:35 +0200

    fix(StoreCheckoutAddressTemplate): use address from localStorage

--- apps/ring/pages/store/checkout/address.tsx
+++ apps/ring/pages/store/checkout/address.tsx
@@ -1,7 +1,9 @@
 import {
   addressCreate,
   CheckoutAddressFormValues,
+  LayoutCheckout,
   StoreCheckoutAddressTemplate,
+  useUserAddress,
 } from '@ring/core/index'
 import { Storyblok } from '@ring/storyblok/services'
 import { GetStaticProps } from 'next'
@@ -26,8 +28,15 @@ export const getStaticProps: GetStaticProps = async ({ locale, preview }) => {
 
 export default function CheckoutAddressPage(): ReactElement | null {
   const router = useRouter()
+  const { address, status } = useUserAddress()
 
-  return <StoreCheckoutAddressTemplate onSubmit={onSubmit} />
+  return (
+    <LayoutCheckout activeStep={0}>
+      {status === 'SUCCESS' ? (
+        <StoreCheckoutAddressTemplate address={address} onSubmit={onSubmit} />
+      ) : null}
+    </LayoutCheckout>
+  )
 
   async function onSubmit(values: CheckoutAddressFormValues) {
     await addressCreate(values.address)
--- libs/ring/core/src/templates/StoreCheckoutAddressTemplate/StoreCheckoutAddressTemplate.tsx
+++ libs/ring/core/src/templates/StoreCheckoutAddressTemplate/StoreCheckoutAddressTemplate.tsx
@@ -4,21 +4,20 @@ import {
   Form,
   FormInputText,
   Grid,
-  LayoutCheckout,
   StoreUserAddress,
-  useUserAddress,
 } from '@ring/core/index'
 import { ReactElement } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
 type CheckoutAddressTemplateProps = {
+  address: StoreUserAddress
   onSubmit: SubmitHandler<CheckoutAddressFormValues>
 }
 
 export function StoreCheckoutAddressTemplate({
+  address,
   onSubmit,
 }: CheckoutAddressTemplateProps): ReactElement {
-  const { address } = useUserAddress()
   const useFormMethods = useForm<CheckoutAddressFormValues>({
     defaultValues: {
       address: {
@@ -35,64 +34,62 @@ export function StoreCheckoutAddressTemplate({
   })
 
   return (
-    <LayoutCheckout activeStep={0}>
-      <Form<CheckoutAddressFormValues>
-        useFormMethods={useFormMethods}
-        onSubmit={onSubmit}
-      >
-        <Grid container alignItems="center">
-          <Grid item xs={12} sm={6}>
-            <FormInputText name="address.email" label="Email" required />
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <FormInputText name="address.firstName" label="Nombre" required />
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <FormInputText name="address.lastName" label="Apellidos" required />
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <FormInputText name="address.address" label="Dirección" required />
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <FormInputText
-              name="address.addressMoreInfo"
-              label="Más información"
-            />
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <FormInputText
-              name="address.postcode"
-              label="Código postal"
-              required
-            />
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <FormInputText name="address.city" label="Población" required />
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <FormInputText
-              name="address.country"
-              label="País"
-              required
-              disabled
-            />
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <Button type="submit" endIcon={<IconArrowForward />} fullWidth>
-              Guardar dirección
-            </Button>
-          </Grid>
+    <Form<CheckoutAddressFormValues>
+      useFormMethods={useFormMethods}
+      onSubmit={onSubmit}
+    >
+      <Grid container alignItems="center">
+        <Grid item xs={12} sm={6}>
+          <FormInputText name="address.email" label="Email" required />
         </Grid>
-      </Form>
-    </LayoutCheckout>
+
+        <Grid item xs={12} sm={6}>
+          <FormInputText name="address.firstName" label="Nombre" required />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <FormInputText name="address.lastName" label="Apellidos" required />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <FormInputText name="address.address" label="Dirección" required />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <FormInputText
+            name="address.addressMoreInfo"
+            label="Más información"
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <FormInputText
+            name="address.postcode"
+            label="Código postal"
+            required
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <FormInputText name="address.city" label="Población" required />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <FormInputText
+            name="address.country"
+            label="País"
+            required
+            disabled
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Button type="submit" endIcon={<IconArrowForward />} fullWidth>
+            Guardar dirección
+          </Button>
+        </Grid>
+      </Grid>
+    </Form>
   )
 }
 
