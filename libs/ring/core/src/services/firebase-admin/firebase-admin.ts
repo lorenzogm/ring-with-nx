a236cf33521a191e917047b5cae49cc950bf0f22
Lorenzo García Moreno <dev@lorenzogm.com>
2021-10-09 12:56:00 +0200

    feat(Store): add firebase-admin

---
+++ libs/ring/core/src/services/firebase-admin/firebase-admin.ts
@@ -0,0 +1,24 @@
+import firebaseAdmin from 'firebase-admin'
+
+if (!firebaseAdmin.apps.length) {
+  const {
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    FIREBASE_PRIVATE_KEY,
+    FIREBASE_CLIENT_EMAIL,
+  } = process.env
+
+  if (!FIREBASE_PRIVATE_KEY) {
+    throw new Error('Undefined "FIREBASE_PRIVATE_KEY"')
+  }
+
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert({
+      projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: FIREBASE_CLIENT_EMAIL,
+    }),
+  })
+}
+
+export default firebaseAdmin
+export const db = firebaseAdmin.firestore()
