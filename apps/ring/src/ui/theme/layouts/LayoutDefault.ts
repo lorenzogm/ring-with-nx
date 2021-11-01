ca824520b2e0a8724111eaa488c1d40626930254
Lorenzo García Moreno <dev@lorenzogm.com>
2021-11-01 21:59:03 +0100

    fix LayoutDefault

--- apps/ring/src/ui/theme/layouts/LayoutDefault.ts
+++ apps/ring/src/ui/theme/layouts/LayoutDefault.ts
@@ -4,11 +4,4 @@ export const LayoutDefault: RingTheme['layouts']['LayoutDefault'] = {
   Container: {
     minHeight: 'calc(100vh - 88px)',
   },
-  AsideBody:{
-
-  display: flex;
-  min-height: calc(100vh - 245px);
-  flex-direction: column;
-  margin-bottom: ${({ theme }) => theme.spacing(6)}px;
-  }
 }
